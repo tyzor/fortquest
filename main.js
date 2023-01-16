3002b8fb87ce4a946f53ef27164fe24389d4acd9
tyzor <bwnallan@gmail.com>
2023-01-16 17:04:14 -0500

    added target rolling for map

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+const mapOrigX = 780;
+const mapOrigY = 843;
+
 document.body.onload = function () {
     console.log('loaded')
+
+    let targetEl = document.querySelector('#target');
+    let rollBtn = document.querySelector('#rollButton');
+    targetEl.style.opacity = 0
+
+    rollBtn.onclick = (evt) => {
+        setNewTarget(targetEl)
+    }
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setNewTarget(targetEl)
+{
+    let x = getRandomInt(100,mapOrigX-100)
+    let y = getRandomInt(100,mapOrigY-100)
+    let pos = getScaledCoords(x,y)
+    
+    let props = window.getComputedStyle(targetEl, null)
+    let tw = parseInt(props.width)
+    let th = parseInt(props.height)
+
+    targetEl.style.left = Math.floor(pos.x - tw/2) + 'px'
+    targetEl.style.top = Math.floor(pos.y - th/2) + 'px'
+    targetEl.style.opacity = 0.8
+}
+
+function getScaledCoords(x,y) {
+
+    let mapEl = document.querySelector('#map')
+    let xRatio = mapEl.width / mapOrigX;
+    let yRatio = mapEl.height / mapOrigY;
+    
+    return {x: x*xRatio, y: y*yRatio}
+
 }
