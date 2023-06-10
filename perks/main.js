008ee5f94cb2932e4414401e214c31831daca93d
tyzor <74879636+tyzor@users.noreply.github.com>
2023-06-10 14:57:19 -0400

    fix layout on mobile devices, revised the points chart

--- perks/main.js
+++ perks/main.js
@@ -19,6 +19,9 @@ document.body.onload = () => {
         };
     })
 
+    // load points
+    loadPointsSheet(document.getElementById("pointsContainer"));
+
 }
 
 function onClickTab(id) {
@@ -56,4 +59,44 @@ function onClickPerkTree(id)
         } else
             el.classList.add("active");
     })   
+}
+
+// Instead of embedding we can pull sheet data
+async function loadPointsSheet(container) {
+    let url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSMpwEditt1W2w-wOk9SoqpK_uOZ01ARoO_ATg98hnUk2g2fEKc_JHtaP651S-m_K-9P8nznyqzoS0q/pub?gid=1028857396&single=true&output=csv"
+    let response = await fetch(url);
+    let pointsText = await response.text();
+    // Get an array of each line
+    let pointsLines = pointsText.match(/[^\r\n]+/g);
+
+    // First line is players
+    let players = pointsLines[0].split(',');
+    // Second line is scores
+    let scores = pointsLines[1].split(',');
+
+    // Lets assemble our data
+    let playerData = [];
+    players.forEach( (player,index) => {
+        playerData.push({
+            name: player,
+            score: scores[index]
+        })
+    })
+    // Sort from first to last place
+    playerData.sort( (a,b) => b.score-a.score );
+
+
+    // Create the element
+    let pointEl = document.createElement('div');
+    pointEl.id = 'playerPointsChart';
+    playerData.forEach(player => {
+        pointEl.innerHTML += `
+            <div class='pointsChartRow'>
+                <div class='playerName'>${player.name}</div>
+                <div class='playerScore'>${player.score}</div>
+            </div>
+        `;
+    })
+    container.replaceChildren(pointEl);
+
 }
